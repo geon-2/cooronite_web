9bc2686e63813bf65e1290e8f455f4d10a474002
geon-2 <gunle0416@gmail.com>
2025-06-09 22:05:54 +0900

    Feat: Add login code

--- src/pages/Callback.jsx
+++ src/pages/Callback.jsx
@@ -1,15 +1,39 @@
-import {useEffect} from "react";
+import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+
+const SERVER_URL = import.meta.env.VITE_SERVER_API_URL;
+const REDIRECT_URI = 'https://cooronite-web.vercel.app/callback';
 
 function Callback() {
     const location = useLocation();
-    const code = location.code;
+    const query = new URLSearchParams(location.search);
+    const code = query.get("code");
 
     useEffect(() => {
+        const fetchLogin = async () => {
+            const response = await fetch(SERVER_URL + "/api/auth/login", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    code: code,
+                    redirect_uri: REDIRECT_URI
+                })
+            });
 
-    }, []);
+            const { user } = await response.json();
+            if (response.status === 200) {
+                window.ReactNativeWebView?.postMessage(JSON.stringify({ userData: user }));
+            } else {
+                throw new Error("Login Error!!");
+            }
+        };
 
-    return <></>
+        if (code) fetchLogin();
+    }, [code]);
+
+    return <></>;
 }
 
 export default Callback;
