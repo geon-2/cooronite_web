7e00dea3a800d1867667a35377a6bb27f0d2733c
geon-2 <gunle0416@gmail.com>
2025-06-09 18:52:23 +0900

    Feat: Add login process

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,11 @@
 import {useEffect} from "react";
 
 const SERVER_URL = import.meta.env.VITE_SERVER_API_URL
+const REDIRECT_URI = 'https://cooronite-web.vercel.app/callback'
 
 function Login() {
     useEffect(() => {
-        fetch(SERVER_URL + "/api/auth/authorize", {
-            method: "POST",
-            header: {
-                'Content-Type': 'application/json'
-            },
-            data: JSON.stringify({
-                redirect_uri: 'https://cooronite-web.vercel.app/callback'
-            })
-        })
+        window.location.href = SERVER_URL + "/api/auth/authorize?redirect_uri=" + REDIRECT_URI
     }, []);
 
     return <></>
